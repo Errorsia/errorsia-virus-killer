0b149fef8ac639e43721b85cfb4cbd44d30b47fc
Errorsia <Errorsia@outlook.com>
2025-08-21 20:34:49 +0800

    Version 4.0.0-beta

--- xyzvk_main.py
+++ xyzvk_main.py
@@ -146,3 +146,24 @@ class ErrorsiaVirusKillerApp:
                 self.build_Log = False
                 # tk.messagebox.showerror("PermissionError")
         self.logic.write_log_config(self.build_Log)
+
+
+if __name__ == '__main__':
+    ErrorsiaVirusKillerApp()
+
+# Project Name: [你的项目名称]
+# Author: [你的名字或组织名] <[你的电子邮件或网站]>
+# Copyright (C) [年份] [你的名字或组织名]
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
