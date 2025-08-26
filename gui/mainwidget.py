8864ccaba512fa40825d35a7a5a3f6bcec4e4761
Errorsia <Errorsia@outlook.com>
2025-08-26 17:29:54 +0800

    Version 4.0.0-alpha

---
+++ gui/mainwidget.py
@@ -0,0 +1,5 @@
+from PySide6.QtWidgets import (
+    QWidget, QPushButton, QLabel, QTextEdit,
+    QComboBox, QGridLayout, QVBoxLayout, QHBoxLayout, QApplication  # , QFrame
+)
+from PySide6.QtCore import Qt
