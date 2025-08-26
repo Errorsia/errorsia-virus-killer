affd692b217f716c295a3f0285e36650f6baf9db
Errorsia <Errorsia@outlook.com>
2025-08-26 17:30:14 +0800

    Version 4.0.0-alpha

--- gui/mainwidget.py
+++ gui/mainwidget.py
@@ -3,3 +3,17 @@ from PySide6.QtWidgets import (
     QComboBox, QGridLayout, QVBoxLayout, QHBoxLayout, QApplication  # , QFrame
 )
 from PySide6.QtCore import Qt
+
+class MainWidget(QWidget):
+    def __init__(self, var, logic, build_log):
+        super().__init__()
+        self.var = var
+        self.logic = logic
+        self.build_log = build_log
+        self.label1 = None
+        # self.debug_frame = self.debug_combobox1 = self.output_text = None
+        self.debug_layout = self.debug_combobox1 = self.output_text = None
+        self.button1 = self.button2 = self.button3 = self.button4 = self.button5 = None
+        self.widgets = None
+
+        self.init_ui()
