e40d8a38b65515d4a35d77de523f83b9a20fe8a7
Errorsia <Errorsia@outlook.com>
2026-02-09 15:19:10 +0800

    fix: wrong hint

--- evk_main.py
+++ evk_main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Project Name: Errorsia virus killer
-# Version: 4.2.0
+# Version: 4.2.6
 # Authors:
 #   - Ariskanyaa <Ariskanyaa@outlook.com>
 #   - Errorsia <Errorsia@outlook.com>
