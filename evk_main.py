e2667dbdffe06b8dfec1fa286df8daf4011e9175
Errorsia <Errorsia@outlook.com>
2026-02-10 22:43:09 +0800

    update

--- evk_main.py
+++ evk_main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Project Name: Errorsia virus killer
-# Version: 4.2.6
+# Version: 4.x.x
 # Authors:
 #   - Ariskanyaa <Ariskanyaa@outlook.com>
 #   - Errorsia <Errorsia@outlook.com>
